c2eb94eb5b7c0491e968cc0852af66acdb1b97f9
FelipeDNL <felipednlled@gmail.com>
2025-04-04 21:33:57 -0300

    curriculo

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <div className='quadrado'></div>
       </h1>
       <div className='opcoes-header'>
-        <Link className='op-header'>[Meu Currículo]</Link>
+        <a href='/Felipe_Davi_CV.pdf' target="_blank">[Currículo]</a>
         <Link to='https://www.linkedin.com/in/felipednl/' className='op-header' target='_blank'>[LinkedIn]</Link>
         <Link to='/projetos' className='op-header'>[Projetos]</Link> 
         <Link to='https://github.com/FelipeDNL' className='op-header' target='_blank'>[GitHub]</Link>
